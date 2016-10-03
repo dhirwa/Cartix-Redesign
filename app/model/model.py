a953cf212edd3c40f3456bc9af05bddb4b630db2
hirwa dieume <dhirwa@exuus.com>
2016-10-03 14:21:16 +0200

    Another Commit

--- app/model/model.py
+++ app/model/model.py
@@ -20,40 +20,74 @@ class Cartix_province(db.Model):
     ctxp_id = db.Column(db.Integer, primary_key = True)
     ctxp_name = db.Column(db.String(20))
     cartix_district=db.relationship('Cartix_district',backref='cartix_province', lazy='dynamic')
+    cartix_population = db.relationship('Cartix_population',backref='cartix_province',lazy='dynamic')
+    cartix_savinggroup=db.relationship('Cartix_savinggroup',backref='cartix_province', lazy= 'dynamic')
+    cartix_financialInst=db.relationship('Cartix_financialInst',backref='cartix_province', lazy='dynamic')
+    cartix_agent=db.relationship('Cartix_agent',backref='cartix_province',lazy='dynamic')
+
 
 class Cartix_district(db.model):
     ctxd_id = db.Column(db.Integer, primary_key = True)
     ctxd_name = db.column(db.String(20))
     ctxp_pid= db.Column(db.Integer, db.ForeignKey('cartix_province.ctxp_id'))
     cartix_sector=db.relationship('Cartix_sector',backref='cartix_district', lazy='dynamic')
-
+    cartix_population = db.relationship('Cartix_population',backref='cartix_district',lazy='dynamic')
+    cartix_savinggroup=db.relationship('Cartix_savinggroup',backref='cartix_district', lazy= 'dynamic')
+    cartix_financialInst=db.relationship('Cartix_financialInst',backref='cartix_district', lazy='dynamic')
+    cartix_agent=db.relationship('Cartix_agent',backref='cartix_district',lazy='dynamic')
 
 class Cartix_sector(db.model):
     ctxs_id = db.Column(db.Integer, primary_key = True)
     ctxs_name = db.Column(db.)
     ctxd_did=db.Column(db.Integer, db.ForeignKey('cartix_district.ctxd_id'))
-    cartix_savvinggroup=db.relationship('Cartix_savinggroup',backref='cartix_sector', lazy= 'dynamic')
+    cartix_population = db.relationship('Cartix_population',backref='cartix_sector',lazy='dynamic')
+    cartix_savinggroup=db.relationship('Cartix_savinggroup',backref='cartix_sector', lazy= 'dynamic')
+    cartix_financialInst=db.relationship('Cartix_financialInst',backref='cartix_sector', lazy='dynamic')
+    cartix_agent=db.relationship('Cartix_agent',backref='cartix_sector',lazy='dynamic')
+
 
 class Cartix_savinggroup(db.Model):
     ctxsg_id=db.Column(db.Integer, primary_key = True)
     ctxsg_name=db.Column(db.String(50))
     ctxsg_creationyear=db.Column(db.Integer)
     ctxsg_sector=db.Column(db.Integer, db.ForeignKey('cartix_sector.ctxs_id'))
+    ctxsg_district=db.Column(db.Integer, db.ForeignKey('cartix_district.ctxd_id'))
+    ctxsg_province=db.Column(db.Integer, db.Foreignkey('cartix_province.ctxp_id'))
     ctxsg_members=db.Column(db.Integer)
     ctxsg_female=db.Column(db.Integer)
     ctxsg_male=db.Column(db.Integer)
     ctxsg_fundingNgo = db.Column(db.String(50))
     ctxsg_partnerNgo = db.Column(db.String(50))
-    ctxsg_amount=db.Column(db.Decimal)
-    ctxsg_outstLoan=db.Column(db.Decimal)
-
-class Cartix_localNgo(db.Model):
-      ctxln_id=db.column(db.Integer, primary_key=True)
-      ctxln_name=db.Column(db.String(50))
+    ctxsg_amount=db.Column(db.bigint)
+    ctxsg_outstLoan=db.Column(db.bigint)
 
 
-class Cartix_interNgo(db.Model):
-    ctxin_id=db.Column(db.Integer,primary_key = True)
-    ctxin_name=db.Column(db.String(50))
 
-        
+class Cartix_financialInst(db.Model):
+    ctxfn_id= db.Column(db.Integer, primary_key = True)
+    ctxfn_name=db.Colum(db.String(50))
+    ctxfn_type=db.Colum(db.String(50))
+    ctxsg_sector=db.Column(db.Integer, db.ForeignKey('cartix_sector.ctxs_id'))
+    ctxsg_district=db.Column(db.Integer, db.ForeignKey('cartix_district.ctxd_id'))
+    ctxg_province=db.Column(db.Integer, db.Foreignkey('cartix_province.ctxp_id'))
+
+
+
+class Cartix_agent(db.Model):
+    ctxa_id = db.Column(db.Integer, primary_key = True)
+    ctxa_name = db.Column(db.String(50))
+    ctxa_type = db.Column(db.String(50))
+    ctxa_sector=db.Column(db.Integer, db.ForeignKey('cartix_sector.ctxs_id'))
+    ctxa_district=db.Column(db.Integer, db.ForeignKey('cartix_district.ctxd_id'))
+    ctxa_province=db.Column(db.Integer, db.Foreignkey('cartix_province.ctxp_id'))
+
+
+class Cartix_population(db.Model):
+    ctxpp_id=db.Column(db.Integer, primary_key = True)
+    ctxpp_name=db.Column(db.String(50))
+    ctxpp_total=db.Column(db.Integer)
+    ctxpp_male=db.Column(db.Integer)
+    ctxpp_female=db.Column(db.Integer)
+    ctxpp_province = db.Column(db.Integer, db.ForeignKey('cartix_province.ctxp_id'))
+    ctxpp_district = db.Column(db.Integer, db.ForeignKey('cartix_district.ctxd_id'))
+    ctxpp_sector = db.Column(db.Integer, db.ForeignKey('cartix_sector.ctxs_id'))
--- app/model/schema.py
+++ app/model/schema.py
@@ -29,13 +29,43 @@ class Cartix_savinggroupSchema(Schema):
     ctxsg_name= fields.String()
     ctxsg_creationyear= fields.Integer()
     ctxsg_sector= fields.Integer()
+    ctxsg_district= fields.Integer()
+    ctxsg_province= fields.Integer()
     ctxsg_members=fields.Integer()
     ctxsg_female=fields.Integer()
     ctxsg_male=fields.Integer()
     ctxsg_fundingNgo = fields.String()
     ctxsg_partnerNgo = fields.String()
-    ctxsg_amount= fields.Decimal()
-    ctxsg_outstLoan= fields.Decimal()
+    ctxsg_amount= fields.bigint()
+    ctxsg_outstLoan= fields.bigint()
 
 ctxsg_schema=Cartix_savinggroupSchema()
 ctxgss_schema=Cartix_savinggroupSchema(many=True)
+
+
+
+class Cartix_financialInstSchema(Schema):
+    ctxfn_id= fields.Integer(dump_only = True)
+    ctxfn_name=fields.String(50)
+    ctxfn_type=fields.String(50)
+
+ctxfn_schema=Cartix_financialInstSchema()
+ctxfns_schema=Cartix_financialInstSchema(many=True)
+
+class Cartix_agentSchema(Schema):
+    ctxa_id = fields.Integer(dump_only = True)
+    ctxa_name = fields.String(50)
+    ctxa_type = fields.String(50)
+
+ctxa_schema=Cartix_agentSchema()
+ctxas_schema=Cartix_agentSchema(many=True)
+
+class Cartix_populationSchema(Schema):
+    ctxpp_id=fields.Integer(dump_only = True)
+    ctxpp_name=fields.String(50)
+    ctxpp_total=fields.Integer()
+    ctxpp_male=fields.Integer()
+    ctxpp_female=fields.Integer()
+
+ctxpp_schema=Cartix_populationSchema()
+ctxpps_schema=Cartix_populationSchema(many=True)
